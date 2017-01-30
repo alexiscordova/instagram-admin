797f43ec2b8a3cadec79a5c6adcfe833c2fc83d8
Alexis Córdova <acordova@mac.com>
2017-01-30 10:31:33 -0800

    Change Application structure to class syntax

--- app/app.jsx
+++ app/app.jsx
@@ -10,16 +10,11 @@ import GlobalHeader from './components/global-header/index.jsx';
 import GlobalFooter from './components/global-footer/index.jsx';
 
 // Base Application Structure
-function Application() {
-  return (
-    <section className="react-root">
-      <GlobalHeader />
-      <main className="wrapper">
-        <h1>Hello, World!</h1>
-      </main>
-      <GlobalFooter />
-    </section>
-  );
-}
+export default class Application extends React.Component {
+  render() {
+    return (
+    )
+  }
+};
 
 ReactDOM.render(<Application />, document.querySelector('#app'));
