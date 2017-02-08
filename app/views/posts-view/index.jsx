b5bc2be90aaad631c3428deac76b0a5e78f01781
Alexis Córdova <acordova@mac.com>
2017-02-08 15:56:36 -0800

    Add standardized wrapper class

--- app/views/posts-view/index.jsx
+++ app/views/posts-view/index.jsx
@@ -8,10 +8,10 @@ import PostsContainer from '../../containers/posts-container/index.jsx';
 class PostsView extends React.Component {
   render() {
     return (
-      <div>
+      <section className="content">
         <ProfileInfoContainer />
         <PostsContainer />
-      </div>
+      </section>
     );
   }
 };
