9cecfc1172ee19be92b59d95fd53a7db3877989f
Alexis Córdova <acordova@mac.com>
2017-02-10 13:09:58 -0800

    Add Instagram oAuth check

--- app/router.jsx
+++ app/router.jsx
@@ -8,14 +8,52 @@ import ProfileView from './views/profile-view/index.jsx';
 import PostsView from './views/posts-view/index.jsx';
 import LoginView from './views/login-view/index.jsx';
 
+function isLoggedIn() {
+  // TODO probably replace this mess with some Redux magic, you buffoon
+
+  // check if token is in url
+  if (window.location.hash.length) {
+    let token = window.location.hash.split('=')[1],
+      storageData = {
+        key: token,
+        expiration: new Date().getTime() + 60480000
+      };
+
+    // Set token data in localStorage
+    window.localStorage.setItem('token', JSON.stringify(storageData));
+
+    // Remove window.location.hash from URL
+    history.pushState('', document.title, window.location.pathname + window.location.search);
+
+    // Return
+    return true;
+  } else if (typeof window.localStorage.token != 'undefined') {
+    let tokenExpirationTime = JSON.parse(window.localStorage.token).expiration,
+      currentTime = new Date().getTime();
+
+    // Check if token in localStorage is still valid
+    return (tokenExpirationTime > currentTime) ? true : false
+  } else {
+    return false;
+  }
+}
+
+function requireAuth(nextState, replace) {
+  if (!isLoggedIn()) {
+    replace({
+      pathname: '/login'
+    });
+  }
+}
+
 // Routes
 const routes = (
-  <Router history={browserHistory}>
-    <Route component={AppView}>
-      <Route path="/" component={ProfileView} />
-      <Route path="/posts" component={PostsView} />
-      <Route path="/liked" component={PostsView} />
-      {/*<Route path="/login" component={LoginView} />*/}
+  <Router history={ browserHistory }>
+    <Route component={ AppView }>
+      <Route path="/" component={ ProfileView } onEnter={ requireAuth } />
+      <Route path="/posts" component={ PostsView } onEnter={ requireAuth } />
+      <Route path="/liked" component={ PostsView } onEnter={ requireAuth } />
+      <Route path="/login" component={ LoginView } contentClass="u-centered" />
     </Route>
   </Router>
 );
