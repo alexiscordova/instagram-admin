b730df9612672a6ff114148d3d7b5bff0f402cae
Alexis Córdova <acordova@mac.com>
2017-02-10 23:14:10 -0800

    Add minor bug fixes

--- app/components/more-content/index.jsx
+++ app/components/more-content/index.jsx
@@ -9,7 +9,7 @@ class MoreContent extends React.Component {
   render() {
     return (
       <span className="more_content">
-        <Link className="more_link" to={ this.props.path }>{ this.props.text }</Link>
+        <Link className="more_link" to={this.props.path}>{this.props.text}</Link>
       </span>
     );
   }
