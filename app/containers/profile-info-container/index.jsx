2fcc1f3170049a2ec60f071a75379dc02e1ee5f9
Alexis Córdova <acordova@mac.com>
2017-02-10 23:13:46 -0800

    Get profile info data from Instagram API

--- app/containers/profile-info-container/index.jsx
+++ app/containers/profile-info-container/index.jsx
@@ -6,9 +6,43 @@ import fetchJsonp from 'fetch-jsonp';
 import ProfileInfo from '../../components/profile-info/index.jsx';
 
 class ProfileInfoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        counts: {
+          media: '',
+          follows: '',
+          followed_by: ''
+        }
+      }
+    }
+  }
+
+  loadProfileInfo(token) {
+    let url = `https://api.instagram.com/v1/users/self/?access_token=${token}`;
+
+    fetchJsonp(url).then((response) => {
+      return response.json();
+    }).then((data) => {
+      if (data.meta.code === 200) {
+        this.setState({
+          data: data.data
+        });
+      }
+    });
+  }
+
+  componentWillMount() {
+    let token = JSON.parse(window.localStorage.token).key;
+
+    this.loadProfileInfo(token);
+  }
+
   render() {
     return (
-      <ProfileInfo />
+      <ProfileInfo data={this.state.data} />
     );
   }
 };
